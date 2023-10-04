135ebece8dee178275c19c2fa0c451c6463e7271
Hideki Katagiri <exthirix@gmail.com>
2023-10-05 00:26:00 +0900

    add some library (#3)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
+import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ babel: { plugins: [] } })],
-})
+  appType: "spa",
+  clearScreen: false,
+  logLevel: "info",
+  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } })],
+  resolve: {
+    extensions: [".mjs", ".js", ".mts", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  server: {
+    port: 8080,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    assetsInlineLimit: 4096,
+    cssCodeSplit: true,
+    sourcemap: true, // prd では不要
+    emptyOutDir: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/lib/vitest/setup.ts",
+  },
+});
