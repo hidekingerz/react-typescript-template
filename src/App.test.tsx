c2e63fbbe4a9e886d3a9799bcada58f717aa177b
hidekingerz <exthirix@gmail.com>
2023-11-18 02:47:11 +0900

    ロゴを追加してテストを追記

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,16 @@ import { render, screen } from "./lib/vitest/test-util.ts";
 import { App } from "./App.tsx";
 
 describe("App test", () => {
-  it("the title is visible", () => {
+  const setup = () => {
     render(<App />);
+  };
+
+  it("ロゴが表示される", () => {
+    setup();
+    expect(screen.getByRole("img", { name: "logo" })).toBeDefined();
+  });
+  it("指定のタイトルが表示される", () => {
+    setup();
     expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import logo from "../src/assets/react.svg";
 
 function App() {
   return (
     <>
+      <img src={logo} height={350} width={200} alt={"logo"} />
       <div>Hello Vite + React!</div>
     </>
   );
