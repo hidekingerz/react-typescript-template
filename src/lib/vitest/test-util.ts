64ba380960736f9d12e72cf523b37fb2eb5e6879
Hideki Katagiri <exthirix@gmail.com>
2025-01-13 21:30:18 +0900

    eslint flatconfig対応 (#652)
    * eslint flatconfig対応
    
    * fix

--- src/lib/vitest/test-util.ts
+++ src/lib/vitest/test-util.ts
@@ -1,11 +1,12 @@
 import { cleanup, render } from "@testing-library/react";
+import { ReactElement } from "react";
 import { afterEach } from "vitest";
 
 afterEach(() => {
   cleanup();
 });
 
-function customRender(ui: React.ReactElement, options = {}) {
+function customRender(ui: ReactElement, options = {}) {
   return render(ui, {
     // wrap provider(s) here if needed
     wrapper: ({ children }) => children,
